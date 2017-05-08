20b3bb063ebfdf6283d3fadadbd418c3d8bac63e
Metin KOC <saucompeng@gmail.com>
2017-05-08 12:59:50 +0300

    Create ppp-creator.sh

---
+++ tutorials/tutorial2/ppp-creator.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+echo "
+ABORT "BUSY"
+ABORT "NO CARRIER"
+ABORT "NO DIALTONE"
+ABORT "ERROR"
+ABORT "NO ANSWER"
+TIMEOUT 30
+"" AT
+OK ATE0
+OK ATI;+CSUB;+CSQ;+CPIN?;+COPS?;+CGREG?;&D2
+# Insert the APN provided by your network operator, default apn is $1
+OK AT+CGDCONT=1,\"IP\",\"\\T\",,0,0
+OK ATD*99#
+CONNECT" > /etc/chatscripts/quectel-chat-connect
+
+
+echo "
+ABORT \"ERROR\"
+ABORT \"NO DIALTONE\"
+SAY \"\nSending break to the modem\n\"
+""  +++
+""  +++
+""  +++
+SAY \"\nGoodbay\n\"" > /etc/chatscripts/quectel-chat-disconnect
+
+
+echo "
+/dev/$2 115200
+# The chat script, customize your APN in this file
+connect 'chat -s -v -f /etc/chatscripts/quectel-chat-connect'
+# The close script
+disconnect 'chat -s -v -f /etc/chatscripts/quectel-chat-disconnect -T $1'
+# Hide password in debug messages
+hide-password
+# The phone is not required to authenticate
+noauth
+# Debug info from pppd
+debug
+# If you want to use the HSDPA link as your gateway
+defaultroute
+# pppd must not propose any IP address to the peer
+noipdefault
+# No ppp compression
+novj
+novjccomp
+noccp
+ipcp-accept-local
+ipcp-accept-remote
+local
+# For sanity, keep a lock on the serial line
+lock
+modem
+dump
+nodetach
+# Hardware flow control
+nocrtscts
+remotename 3gppp
+ipparam 3gppp
+ipcp-max-failure 30
+# Ask the peer for up to 2 DNS server addresses
+usepeerdns" > /etc/ppp/peers/gprs
+
+
+echo "
+auto gprs
+iface gprs inet ppp
+provider gprs" >> /etc/network/interfaces
